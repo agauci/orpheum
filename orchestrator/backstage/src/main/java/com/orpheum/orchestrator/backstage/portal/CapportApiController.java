91eea92bb6407f6d8ba73bc66a24bd3f364d929d
Andrew Gauci <gauciandrew@gmail.com>
2025-04-12 22:09:38 +0200

    Added some further detail to the capport api

--- orchestrator/backstage/src/main/java/com/orpheum/orchestrator/backstage/portal/CapportApiController.java
+++ orchestrator/backstage/src/main/java/com/orpheum/orchestrator/backstage/portal/CapportApiController.java
@@ -1,5 +1,8 @@
 package com.orpheum.orchestrator.backstage.portal;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +15,18 @@ import java.util.Map;
 public class CapportApiController {
 
     @GetMapping("/captive-portal")
-    public Map<String, Object> getCapportInfo() {
+    public ResponseEntity<Map<String, Object>> getCapportInfo() {
         Map<String, Object> response = new HashMap<>();
         response.put("captive", true); // true if restricted access
         response.put("user-portal-url", "https://orpheum.backstage.cloud");
         response.put("venue-info-url", "https://orpheum.com.mt");
         response.put("can-extend-session", false);
 
-        return response;
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/captive+json");
+        headers.add("Cache-Control", "private");
+
+        return new ResponseEntity<>(response, headers, HttpStatus.OK);
     }
 
 }
