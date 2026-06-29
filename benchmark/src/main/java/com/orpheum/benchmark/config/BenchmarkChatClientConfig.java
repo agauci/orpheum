4b684761055f9bc6663da25306b51f177b7d9fd5
Andrew Gauci <gauciandrew@gmail.com>
2026-06-29 17:57:53 +0200

    Minor fix

--- benchmark/src/main/java/com/orpheum/benchmark/config/BenchmarkChatClientConfig.java
+++ benchmark/src/main/java/com/orpheum/benchmark/config/BenchmarkChatClientConfig.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+
 @Configuration
 public class BenchmarkChatClientConfig {
 
@@ -18,6 +20,7 @@ public class BenchmarkChatClientConfig {
         return OpenAiChatModel.builder()
                 .options(OpenAiChatOptions.builder()
                         .apiKey(apiKey)
+                        .timeout(Duration.ofMinutes(60))
                         .build())
                 .build();
     }
